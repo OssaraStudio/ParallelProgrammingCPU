a4cdfc2eec03386ec95af3b6c2a0ac8be2863f72
Ossara DJAODOH <ossara.djaodoh@mines-paristech.fr>
2024-11-23 21:17:31 +0100

    8

--- TPs/ParallelProgrammingTP/src/TP2/spmv_mpi.cpp
+++ TPs/ParallelProgrammingTP/src/TP2/spmv_mpi.cpp
@@ -215,7 +215,7 @@ int main(int argc, char** argv)
       {
         size_t local_nrows = local_size ;
         if(i < rest) local_nrows ++ ;
-        std::vector<double> local_y(25) ;
+        std::vector<double> local_y(local_nrows+1) ;
 
         MPI_Recv(local_y.data(), local_nrows, MPI_DOUBLE, i, 6, MPI_COMM_WORLD, &status) ;
         // fuse_y.insert(fuse_y.end(), local_y.begin(), local_y.end()) ;
