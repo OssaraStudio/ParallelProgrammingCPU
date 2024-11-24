f5dc621e28859b1f047f9b7a8778b662eb4c6c51
Ossara DJAODOH <ossara.djaodoh@mines-paristech.fr>
2024-11-24 17:36:02 +0100

    8

--- TPs/ParallelProgrammingTP/src/MatrixVector/DenseMatrix.h
+++ TPs/ParallelProgrammingTP/src/MatrixVector/DenseMatrix.h
@@ -190,10 +190,12 @@ namespace PPTP
                 {
                   for (std::size_t irow = start_row; irow < end_row; ++irow)
                   {
+                    double const* matrix_ptr = m_values.data() ;
+                    matrix_ptr += (irow*m_nrows) ;
                     double value = 0;
                     for (std::size_t jcol = start_col; jcol < end_col; ++jcol)
                     {
-                      value += m_values[irow * m_nrows + jcol]*x[jcol];
+                      value += matrix_ptr[jcol]*x[jcol];
                     }
                     y[irow] += value;
                   }
