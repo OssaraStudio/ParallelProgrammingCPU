4247b1dc30e268987e41c2dc600225c582fa9055
Ossara DJAODOH <ossara.djaodoh@mines-paristech.fr>
2024-12-07 23:47:51 +0100

    LiveOfBoids

---
+++ TPs/ParallelProgrammingTP/src/LiveOfBoids/Boid.h
@@ -0,0 +1,60 @@
+/*
+ * Boid.h
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: ossaradj
+ */
+
+#ifndef SRC_MATRIXVECTOR_BOID_H_
+#define SRC_MATRIXVECTOR_BOID_H_
+
+#include "Vector2D.h"
+#include <vector>
+
+
+class Boid {
+public:
+    // Constructor
+    Boid(Vector2D position = Vector2D(), Vector2D velocity = Vector2D())
+    : m_position(position), m_velocity(velocity) {};
+
+    // Overloading stream operator
+    friend std::ostream& operator<<(std::ostream& out, const Boid& b)
+    {
+        out << "position = " << b.m_position << " velocity = " << b.m_velocity;
+        return out;
+    };
+
+    // Getters
+    Vector2D getPosition() const { return m_position; };
+    float getPositionX() const { return m_position.getX(); };
+    float getPositionY() const { return m_position.getY(); };
+    Vector2D getVelocity() const { return m_velocity; };
+    float getVelocityX() const { return m_velocity.getX(); };
+    float getVelocityY() const { return m_velocity.getY(); };
+
+    // Setters
+    void setPosition(Vector2D const& position) { this->m_position = position; };
+    void setPositionX(const float x) { this->m_position.setX(x); };
+    void setPositionY(const float y) { this->m_position.setY(y); };
+    void setVelocity(Vector2D const& velocity) { this->m_velocity = velocity; };
+    void setVelocityX(const float x) { this->m_velocity.setX(x); };
+    void setVelocityY(const float y) { this->m_velocity.setY(y); };
+
+    std::vector<Boid> get_neighbors(const std::vector<Boid>& boids, float radius) const
+    {
+        std::vector<Boid> neighbors;
+        for(auto& other: boids)
+        {
+            if(this->m_position.euclidean_dist(other.m_position) < radius)
+            neighbors.push_back(other);
+        }
+        return neighbors;
+    };
+
+protected:
+    Vector2D m_position;
+    Vector2D m_velocity;
+};
+
+#endif // DEF_BOIDS_HPP
---
+++ TPs/ParallelProgrammingTP/src/TP2/boids_nn.cpp
@@ -0,0 +1,114 @@
+/*
+ * boids_nn.cpp
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: ossaradj
+ */
+
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/cmdline.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include "omp.h"
+#include "tbb/tbb.h"
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "Utils/Timer.h"
+
+int main(int argc, char** argv)
+{
+    using namespace boost::program_options ;
+    options_description desc;
+    desc.add_options()
+        ("help", "produce help")
+        ("nb-threads",value<int>()->default_value(0), "nb threads")
+        ("nrows",value<int>()->default_value(0), "matrix size")
+        ("nx",value<int>()->default_value(0), "nx grid size")
+        ("chunk-size",value<int>()->default_value(1), "chunk size")
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+    notify(vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    int nb_threads = vm["nb-threads"].as<int>() ;
+    if(nb_threads>0)
+    {
+        omp_set_num_threads(nb_threads) ;
+        tbb::task_scheduler_init init(nb_threads);
+    }
+    int nb_procs     = omp_get_num_procs() ;
+    std::cout<<"NB PROCS     :"<<nb_procs<<std::endl ;
+    int nb_available_threads = omp_get_max_threads() ;
+    std::cout<<"NB AVAILABLE_THREADS :"<<nb_available_threads<<std::endl ;
+
+    // using namespace PPTP ;
+
+    // Timer timer ;
+    // int nx = vm["nx"].as<int>() ;
+    
+    
+    // std::size_t nrows = matrix.nrows();
+    // {
+    //     std::vector<double> y(nrows);
+    //     {
+    //         Timer::Sentry sentry(timer,"Boids") ;
+    //         matrix.mult(x,y) ;
+    //     }
+    //     double normy = PPTP::norm2(y) ;
+    //     std::cout<<"||y||="<<normy<<std::endl ;
+    // }
+
+    // matrix.setChunkSize(vm["chunk-size"].as<int>()) ;
+    // {
+    //     std::vector<double> y(nrows);
+    //     {
+    //         Timer::Sentry sentry(timer,"OMPTaskBoids") ;
+    //         matrix.omptaskmult(x,y) ;
+    //     }
+    //     double normy = PPTP::norm2(y) ;
+    //     std::cout<<"OMPTask ||y||="<<normy<<std::endl ;
+    // }
+
+    // {
+    //   std::vector<double> y(nrows);
+    //   {
+    //     Timer::Sentry sentry(timer,"OMPTileBoids") ;
+    //     matrix.omptilemult(x,y) ;
+    //   }
+    //   double normy = PPTP::norm2(y) ;
+    //   std::cout<<"OMPTile ||y||="<<normy<<std::endl ;
+    // }
+
+    // {
+    //   std::vector<double> y(nrows);
+    //   {
+    //     Timer::Sentry sentry(timer,"TBBRangeBoids") ;
+    //     matrix.tbbrangemult(x,y) ;
+    //   }
+    //   double normy = PPTP::norm2(y) ;
+    //   std::cout<<"TBBRange ||y||="<<normy<<std::endl ;
+    // }
+
+    // {
+    //   std::vector<double> y(nrows);
+    //   {
+    //     Timer::Sentry sentry(timer,"TBBRange2DBoids") ;
+    //     matrix.tbbrange2dmult(x,y) ;
+    //   }
+    //   double normy = PPTP::norm2(y) ;
+    //   std::cout<<"TBBRange2DTile ||y||="<<normy<<std::endl ;
+    // }
+  
+  timer.printInfo() ;
+  return 0 ;
+}
